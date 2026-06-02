82586011d4c56ec4b33c1684a976a221c29531ae
hoangtuzami <hoangtuzami@gmail.com>
2026-06-02 15:50:56 +0700

    Force manual ack for issue Kafka listeners

--- src/main/java/com/isums/issueservice/configurations/KafkaConsumerConfig.java
+++ src/main/java/com/isums/issueservice/configurations/KafkaConsumerConfig.java
@@ -6,10 +6,13 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.util.backoff.ExponentialBackOff;
 import lombok.extern.slf4j.Slf4j;
 
@@ -72,5 +75,18 @@ public class KafkaConsumerConfig {
         );
 
         return handler;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory,
+            DefaultErrorHandler kafkaErrorHandler
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(kafkaErrorHandler);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
     }
 }
